5f588e26e143c337aeb683ca1d7fd36309eda89b
ksr <ksr@ss001.win.ad.jhu.edu>
2019-06-27 13:03:57 -0400

    Storing old changes.

--- pipeline/contact_localization/matlab/run_localization_fieldtrip_v3.m
+++ pipeline/contact_localization/matlab/run_localization_fieldtrip_v3.m
@@ -1,38 +1,44 @@
 % REFERENCE: http://www.fieldtriptoolbox.org/tutorial/human_ecog
 %% Setup Paths and Directories for Registration Results and Raw EEG Data
 % ------- CHANGE THIS ------
-addpath("/home/adam2392/Documents/Dropbox/fieldtrip-20190129/");
+addpath("/home/ksr/Downloads/fieldtrip-20190615"); %fieldtrip package
+%20190129/");
 % addpath("/Users/adam2392/Dropbox/fieldtrip-20181108");
-addpath("/Users/adam2392/Dropbox/fieldtrip-20190129");
-addpath("/Users/adam2392/Documents/MATLAB/spm12");
+%addpath("/Users/adam2392/Dropbox/fieldtrip-20190129");
+addpath("/home/ksr/Downloads/spm12") % SPM12 pacakge 
+%addpath("/Users/adam2392/Documents/MATLAB/spm12");
 % addpath("../../.lib/fieldtrip-20181108");
 
 % ------- CHANGE THIS ------
 % neuroimaging output data dir
-RESULTS_DIR = '/home/adam2392/hdd/data/neuroimaging/freesurfer_output/';
+RESULTS_DIR = '/home/ksr/Desktop/contact_localization/freesurfer_output';
 % RESULTS_DIR = '/Users/adam2392/Dropbox/phd_research/data/neuroimaging_results/freesurfer_output/outputfiles';
 % RESULTS_DIR = '/Users/adam2392/Downloads/neuroimaging_results/';
 % RESULTS_DIR = '/home/adam2392/hdd/data/neuroimaging/freesurfer_output/";
 
 % ------- CHANGE THIS ------
 % raw data EEG dir
-RECORD_DATADIR = '/home/adam2392/hdd/data/rawdata';
+RECORD_DATADIR = '/home/ksr/Desktop/contact_localization/raw';%'/home/adam2392/hdd/data/rawdata';
 % RECORD_DATADIR = '/Users/adam2392/Downloads/tngpipeline/';
 
 % ------- CHANGE THIS ------
 % subj id to analyze
-subjID = 'umf001';
-center = 'umf';
-modality = 'ieeg';
-dataset_id = strcat('SEIZURE.edf')
+subjID = 'efri06';      % e.g. efri01, pt01, umf001, tvb1
+center = 'efri';         % e.g. cc, efri, nih, etc.
+%modality = 'ieeg';
+%Reading electrode names
+%Here reading from eeg files
+%dataset_id = strcat('SEIZURE.edf')
 %dataset_id = strcat('Seizure.edf')
+data_id= [upper(subjID) '_WAR_SES1_Setup.mat'];
+%Here reading from saved setup files
 %% Initialize Variablees and Parameters
 % run setup of global variables to make tool GUI work
 ft_defaults
 
 % filepath to your raw data (the header of the edf file has the channel
 % labels and stuff)
-rawdatafilepath = fullfile(RECORD_DATADIR, center, subjID, modality, 'edf', dataset_id)
+rawdatafilepath = fullfile(RECORD_DATADIR, subjID, data_id);
 
 % results directory
 SUBJDIR = fullfile(RESULTS_DIR, subjID);
@@ -47,8 +53,8 @@ elec_coords_filepath = fullfile(CTDIR, [subjID '_elec_initialize.mat']);
 % NAME OUTPUT ACCORDINGLY
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % ctimgfile = fullfile(strcat('CT_IN_T1_mgz.nii.gz'))
-ctimgfile = fullfile(strcat('CT.nii.gz'))
-t1imgfile = fullfile(strcat('T1.nii.gz'))
+ctimgfile = fullfile(strcat('CT.nii'));
+t1imgfile = fullfile(strcat('T1.nii'));
 
 %% Read in Original MRI Scan
 t1_img = ft_read_mri(fullfile(MRIDIR, t1imgfile));
@@ -57,11 +63,12 @@ t1_img = ft_read_mri(fullfile(MRIDIR, t1imgfile));
 ct_img = ft_read_mri(fullfile(CTDIR, ctimgfile));
 
 %% Electrode Placement - Runs GUI
-hdr = ft_read_header(rawdatafilepath);
+%EEG labels
+%hdr = ft_read_header(rawdatafilepath);
 
 % configuration for your GUI to read
 cfg         = [];
-cfg.channel = hdr.label;
+cfg.channel =elec_name;%Labels from analyzed data not eeg. If from eeg file hdr.label;
 elecf = ft_electrodeplacement(cfg, ct_img);
 
 % print out the structure that stores your electrode contact points - xyz
