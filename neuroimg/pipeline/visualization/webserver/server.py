7f037861136f26c04a2d71a24c481ab87d8a9313
Adam Li <adam2392@gmail.com>
2019-11-25 13:54:02 -0500

    Adding things into dev/merge.

---
+++ neuroimg/pipeline/visualization/webserver/server.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, Response, send_file
+import os
+app = Flask(__name__)
+
+app._static_folder = os.path.abspath("templates/static")
+
+
+@app.route("/")
+def home():
+    return render_template("threeD.html")
+
+@app.route('/brain')
+def api_articles():
+
+	return send_file('./templates/static/reconstruction.glb')
+
+if __name__ == "__main__":
+    app.run(debug=True)
