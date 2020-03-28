c79903c9374db0b6aac4a543e43224da50ba7fac
Adam Li <adam2392@gmail.com>
2020-03-27 21:20:11 -0400

    Run la09 on workstation.g

--- neuroimg/pipeline/04-contact_localization/matlab/run_localization_fieldtrip.m
+++ neuroimg/pipeline/04-contact_localization/matlab/run_localization_fieldtrip.m
@@ -5,10 +5,10 @@ clc
 % REFERENCE: http://www.fieldtriptoolbox.org/tutorial/human_ecog
 %% Setup Paths and Directories for Registration Results and Raw EEG Data
 % ------- CHANGE THIS ------
-addpath("/home/ksr/Downloads/fieldtrip-20190615"); %fieldtrip package
-addpath("/home/ksr/Downloads/spm12") % SPM12 pacakge 
+% addpath("/home/ksr/Downloads/fieldtrip-20190615"); %fieldtrip package
+% addpath("/home/ksr/Downloads/spm12") % SPM12 pacakge 
 % addpath("/Users/adam2392/Dropbox/fieldtrip-20181108");
-addpath("/Users/adam2392/Dropbox/fieldtrip-20190129");
+addpath("/Users/adam2392/Dropbox/fieldtrip-20190129/");
 
 % ------- CHANGE THIS ------
 % neuroimaging output data dir
@@ -20,18 +20,34 @@ RESULTS_DIR = fullfile('/Users/adam2392/Dropbox/epilepsy_bids/derivatives/freesu
 % ------- CHANGE THIS ------
 % raw data EEG dir
 RECORD_DATADIR = '/home/ksr/Desktop/contact_localization/freesurfer_output/epilepsy/'; % '/home/ksr/Desktop/contact_localization/freesurfer_output/epilepsy/';
+RECORD_DATADIR = '/Users/adam2392/Dropbox/epilepsy_bids/sourcedata/cleveland/';
+
+% run setup of global variables to make tool GUI work
+ft_defaults
 
 % ------- CHANGE THIS ------
 % subj id to analyze
-subjID = 'nl12';      % e.g. efri01, pt01, umf001, tvb1
-your_initials = 'CH';
+subjID = 'la09';      % e.g. efri01, pt01, umf001, tvb1
+your_initials = 'AL';
+
+% results directory
+SUBJDIR = fullfile(RESULTS_DIR, subjID);
+CTDIR = fullfile(SUBJDIR, 'CT');
+MRIDIR = fullfile(SUBJDIR, 'mri');
 
 % Reading electrode names, or reading from eeg files -> get channel names 
 % data_id= [upper(subjID) '_WAR_SES1_Setup.mat']; 
-data_id = [subjID '_sz_6p_chanlabels.mat'];
-chandata = load(fullfile(RECORD_DATADIR, data_id)); % load(fullfile('/home/ksr/Desktop/contact_localization/raw/', data_id));
-elec_names = chandata.chanlabels;
-elec_labels = cellstr(elec_names);
+% data_id = [subjID '_sz_6p_chanlabels.mat'];
+% chandata = load(fullfile(RECORD_DATADIR, data_id)); % load(fullfile('/home/ksr/Desktop/contact_localization/raw/', data_id));
+% elec_names = chandata.chanlabels;
+% elec_labels = cellstr(elec_names);
+
+% filepath to your raw data (the header of the edf file has the channel
+% labels and stuff)
+data_id = [subjID '_ictal.edf'];
+rawdatafilepath = fullfile(RECORD_DATADIR, subjID,  data_id); % if using .mat file
+rawdatafilepath = fullfile(RECORD_DATADIR, subjID, 'seeg', 'edf', data_id); % if using .edf file
+
 % for i=1:size(elec_names,2)
 %     elec_labels{i} = convertCharsToStrings(elec_names(:, i)); %, '\W', '');
 % end
@@ -39,18 +55,6 @@ elec_labels = cellstr(elec_names);
 % size(elec_names, 1), size(elec_names, 2));
 
 %% Initialize Variablees and Parameters
-% run setup of global variables to make tool GUI work
-ft_defaults
-
-% filepath to your raw data (the header of the edf file has the channel
-% labels and stuff)
-rawdatafilepath = fullfile(RECORD_DATADIR, subjID, data_id);
-
-% results directory
-SUBJDIR = fullfile(RESULTS_DIR, subjID);
-CTDIR = fullfile(SUBJDIR, 'CT');
-MRIDIR = fullfile(SUBJDIR, 'mri');
-
 % output filepath for your electrode localization
 elec_coords_filepath = fullfile(SUBJDIR, 'elecs', [subjID your_initials '_elec_initialize.mat']);
 
@@ -70,12 +74,12 @@ ct_img = ft_read_mri(fullfile(CTDIR, ctimgfile));
 
 %% Electrode Placement - Runs GUI
 % EEG labels read from an eeg file
-% rawdatafilepath = './la04_ictal.edf';
-% hdr = ft_read_header(rawdatafilepath);
+hdr = ft_read_header(rawdatafilepath);
 
 % configuration for your GUI to read
 cfg         = [];
-cfg.channel = elec_labels; % Labels from analyzed data not eeg. If from eeg file hdr.label;
+% cfg.channel = elec_labels; % Labels from analyzed data not eeg. 
+cfg.channel = hdr.label;   %  If from eeg file hdr.label;
 elecf = ft_electrodeplacement(cfg, ct_img);
 
 % print out the structure that stores your electrode contact points - xyz
